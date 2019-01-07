769d65f906cbd5382c160cbb03d262757e7f3c6c
TalkWithKeyboard <songwei@ruguoapp.com>
2019-01-07 16:35:00 +0800

    Fix

--- storm-supervisor/add_newrelic.sh
+++ storm-supervisor/add_newrelic.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+mkdir $STORM_HOME/newrelic
 cp /opt/newrelic.jar $STORM_HOME/newrelic/newrelic.jar
 cp /opt/newrelic.yml $STORM_HOME/newrelic/newrelic.yml
 sudo chmod 777 opt
--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
-sed -i -e "s/%storm%/$STORM_HOME/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s|%storm%|$STORM_HOME|g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
 echo 'storm.zookeeper.root:  "/storm_temporary"' >> $STORM_HOME/conf/storm.yaml
