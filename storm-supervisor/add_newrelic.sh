d61eac4223bd566299453a811cfec8d3c499e1fc
TalkWithKeyboard <songwei@ruguoapp.com>
2019-01-07 14:57:03 +0800

    将 「newrelic.jar」移至 supervisor 中

---
+++ storm-supervisor/add_newrelic.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cp /opt/newrelic.jar $STORM_HOME/newrelic/newrelic.jar
+cp /opt/newrelic.yml $STORM_HOME/newrelic/newrelic.yml
+sudo chmod 777 opt
+sudo chmod 777 $STORM_HOME
+sudo chmod 777 $STORM_HOME/newrelic
+sudo chmod 777 $STORM_HOME/newrelic/newrelic.jar
--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -4,13 +4,6 @@ sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%storm%/$STORM_HOME/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
-echo 'storm.zookeeper.root:  "/storm_test"' >> $STORM_HOME/conf/storm.yaml
-sudo aws s3 cp s3.cn-north-1.amazonaws.com.cn/jike-data-warehouse/newrelic/newrelic.jar $STORM_HOME/newrelic/newrelic.jar
-sudo aws s3 cp s3.cn-north-1.amazonaws.com.cn/jike-data-warehouse/newrelic/newrelic.yml $STORM_HOME/newrelic/newrelic.yml
-sudo chmod 777 opt
-sudo chmod 777 $STORM_HOME
-sudo chmod 777 $STORM_HOME/newrelic
-sudo chmod 777 $STORM_HOME/newrelic/newrelic.jar
-
+echo 'storm.zookeeper.root:  "/storm_temporary"' >> $STORM_HOME/conf/storm.yaml
 
 /usr/sbin/sshd && supervisord
