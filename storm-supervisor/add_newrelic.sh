5415399d1a2aa725ae8a62f6782f3f36cc50353e
TalkWithKeyboard <songwei@ruguoapp.com>
2019-01-17 20:09:29 +0800

    修复添加 new relic 的部分

--- storm-supervisor/add_newrelic.sh
+++ storm-supervisor/add_newrelic.sh
@@ -2,7 +2,9 @@
 mkdir $STORM_HOME/newrelic
 cp /opt/newrelic.jar $STORM_HOME/newrelic/newrelic.jar
 cp /opt/newrelic.yml $STORM_HOME/newrelic/newrelic.yml
-sudo chmod 777 opt
+sudo chmod 777 /opt
 sudo chmod 777 $STORM_HOME
 sudo chmod 777 $STORM_HOME/newrelic
 sudo chmod 777 $STORM_HOME/newrelic/newrelic.jar
+
+echo "worker.childopts: \"-javaagent:$STORM_HOME/newrelic/newrelic.jar -Dnewrelic.environment=LOCAL_TEST\"" >> $STORM_HOME/conf/storm.yaml
