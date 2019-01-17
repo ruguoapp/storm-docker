22d4a8533593aa72a2d40c070c84b29a64bb9e69
TalkWithKeyboard <songwei@ruguoapp.com>
2019-01-17 17:29:46 +0800

    去除nimbus的多余操作

--- storm-nimbus/start-supervisor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-export IP=`hostname -i`
-sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
-sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
-
-echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
-echo 'storm.zookeeper.root:  "/storm_test"' >> $STORM_HOME/conf/storm.yaml
-
-/usr/sbin/sshd && supervisord
