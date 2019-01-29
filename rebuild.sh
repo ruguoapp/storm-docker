2d8bc304004b0372d95736e913484e97d028a216
TalkWithKeyboard <songwei@ruguoapp.com>
2019-01-29 15:02:11 +0800

    [1.0]添加版本管理，修复权限

--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-docker build -t="804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/base" base
-docker build -t="804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm" storm
-docker build -t="804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-nimbus" storm-nimbus
-docker build -t="804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-supervisor" storm-supervisor
-docker build -t="804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-ui" storm-ui
+version=1.0
 
-docker push 804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/base
-docker push 804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm
-docker push 804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-nimbus
-docker push 804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-supervisor
-docker push 804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-ui
+docker build --build-arg version=${version} -t="804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/base:${version}" base
+docker build --build-arg version=${version} -t="804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm:${version}" storm
+docker build --build-arg version=${version} -t="804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-nimbus:${version}" storm-nimbus
+docker build --build-arg version=${version} -t="804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-supervisor:${version}" storm-supervisor
+docker build --build-arg version=${version} -t="804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-ui:${version}" storm-ui
+
+docker push 804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/base:${version}
+docker push 804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm:${version}
+docker push 804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-nimbus:${version}
+docker push 804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-supervisor:${version}
+docker push 804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/storm-ui:${version}
--- storm-nimbus/start-supervisor.sh
+++ storm-nimbus/start-supervisor.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+export IP=`hostname -i`
 sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
-sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s|%zookeeper_path%|$ZK_PATH|g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: \"`hostname -i`\"" >> $STORM_HOME/conf/storm.yaml
